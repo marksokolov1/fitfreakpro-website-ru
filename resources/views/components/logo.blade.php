9eddce6cbe20aeb36c3574e5be0974cd3eee8297
marksokolov1 <92471950+marksokolov1@users.noreply.github.com>
2026-07-24 14:35:51 -0400

    1

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -5,9 +5,9 @@
 
 <img
     {{ $attributes->class([$footer ? 'footer-logo' : 'brand-mark']) }}
-    src="{{ asset('assets/img/fitfreak-logo.png') }}"
-    width="512"
-    height="512"
+    src="{{ asset('assets/img/fitfreak-logo.webp') }}"
+    width="1024"
+    height="1024"
     alt=""
     aria-hidden="true"
     @unless ($priority) loading="lazy" @endunless

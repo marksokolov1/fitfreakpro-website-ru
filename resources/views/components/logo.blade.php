e1e3f865178b0d49ddbc5e3f8b13906a51b9ab05
marksokolov1 <92471950+marksokolov1@users.noreply.github.com>
2026-07-24 14:23:44 -0400

    Replace site logo with transparent wordmark

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -6,13 +6,10 @@
 <img
     {{ $attributes->class([$footer ? 'footer-logo' : 'brand-mark']) }}
     src="{{ asset('assets/img/fitfreak-logo.png') }}"
-    width="640"
-    height="640"
+    width="512"
+    height="512"
     alt=""
     aria-hidden="true"
     @unless ($priority) loading="lazy" @endunless
     @if ($priority) fetchpriority="high" @endif
 >
-@unless ($footer)
-    <span class="brand-word">FitFreak Pro</span>
-@endunless
